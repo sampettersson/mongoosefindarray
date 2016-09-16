bb230efbe2b42535a2fed6d680efdcc81ba60af2
Sams Dator <samsdator@Samss-MacBook-Pro.local>
2016-09-16 14:02:11 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import Promise from 'bluebird';
+import mongoose from 'mongoose';
+
+class MongooseFindArray {
+    array;
+    model;
+
+    constructor(array, model) {
+        if (Array.isArray(array)) {
+            // remove dupes from array with spread and Set
+            this.array = [...new Set(array)];
+        } else {
+            throw new Error("Argument 'array' not an Array.");
+        }
+
+        this.array.map((arrayItem) => {
+            return new mongoose.Types.ObjectId(arrayItem);
+    });
+
+        this.model = model;
+    }
+
+    query() {
+        return new Promise((resolve, reject) => {
+                this.model.find({ _id: { $in: this.array } }, (err, result) => {
+                if (result) {
+                    resolve(result);
+                } else {
+                    reject(err);
+    }
+    });
+    });
+    }
+}
+
+export default MongooseFindArray;
